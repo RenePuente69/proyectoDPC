162186eda8aacf93b12b1a6eccad068e32996653
pato <patriciobmx16@gmail.com>
2024-05-07 12:24:39 -0400

    img de recepcion

--- secciones/recepcion/home.php
+++ secciones/recepcion/home.php
@@ -23,6 +23,7 @@
                     <img src="<?php echo $registro['foto']; ?>" alt="">
                 </figure>
                 <div class="contenido">
+                    <img src="../../assets/img/img3Rece.png" alt="">
                     <h3>ATRASOS</h3>
                     <p>En esta sección puede ingresar los atrasos de los alumnos al ingreso del establecimiento.</p>
                     <a href="">Ingresar aquí</a>
@@ -30,9 +31,10 @@
             </div>
             <div class="card">
                 <figure>
-                    <img src="<?php echo $registro['foto']; ?>" alt="">
+                    <img src="../../assets/img/img2Rece.png" alt="">
                 </figure>
                 <div class="contenido">
+                    <img src="" alt="">
                     <h3>RETIROS</h3>
                     <p>En esta sección se ingresa los retiros del alumnos del establecimiento.</p>
                     <a href="">Ingresar aquí</a>
@@ -43,6 +45,7 @@
                     <img src="<?php echo $registro['foto']; ?>" alt="">
                 </figure>
                 <div class="contenido">
+                    <img src="../../assets/img/img1Rece.png" alt="">
                     <h3>FALTAS</h3>
                     <p>En esta sección se ingresa las falta de mas de un día a clases.</p>
                     <a href="">Ingresar aquí</a>
